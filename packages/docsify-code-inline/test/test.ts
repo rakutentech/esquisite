b7b77529f105d203e8ab4044a0c1a4141b169b9e
Julien Cayzac <julien.cayzac@rakuten.com>
2021-01-20 15:56:25 +0900

    fix(docsify-code-inline): add tests

--- packages/docsify-code-inline/test/test.ts
+++ packages/docsify-code-inline/test/test.ts
@@ -1,5 +1,37 @@
 import test from 'ava';
+import { lint, formatters } from 'stylelint';
 
-test('dummy test', (t) => {
-  t.truthy(true);
+import { DEFAULT_CSS, transform } from '../src';
+
+test(`The default CSS is valid`, async (assert) => {
+  const output = await lint({
+    code: DEFAULT_CSS,
+    config: {
+      extends: `stylelint-config-standard`
+    }
+  });
+
+  assert.false(output.errored, `CSS errors: ${formatters.compact(output.results)}`);
+});
+
+test(`Unmarked code is untouched`, (assert) => {
+  const input = 'This is `code`.';
+  assert.deepEqual(transform(input), input);
+});
+
+test(`escaped code is untouched`, (assert) => {
+  const input = 'This is [`<code>\\` html]`.';
+  assert.deepEqual(transform(input), input);
+});
+
+test(`marked code is transformed`, (assert) => {
+  const input = 'This is [`<code>` html].';
+  const expected = `This is <code class="language-html">&lt;code&gt;</code>.`;
+  assert.deepEqual(transform(input), expected);
+});
+
+test(`transformation isn't greedy`, (assert) => {
+  const input = 'This is [`<code>` html].';
+  const expected = `This is <code class="language-html">&lt;code&gt;</code>.`;
+  assert.deepEqual(transform(input + input), expected + expected);
 });
